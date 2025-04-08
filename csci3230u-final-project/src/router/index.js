8079f04294e51ac4dcfbe7b939c2a3e16be131f0
David <garciadavid2003@gmail.com>
2025-04-07 23:34:51 -0400

    WDF-39 Refactor router configuration to enforce authentication on protected routes and improve navigation guard logic

--- csci3230u-final-project/src/router/index.js
+++ csci3230u-final-project/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserDataStore } from '@/stores/userData.js'
 import HomeView from '../views/HomeView.vue'
 import DashboardView from '@/views/DashboardView.vue'
 import TopSongsView from '@/views/TopSongsView.vue'
@@ -15,46 +16,72 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: false },
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: DashboardView
+      component: DashboardView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/top-songs',
       name: 'top-songs',
-      component: TopSongsView
+      component: TopSongsView,
+      meta: { requiresAuth: true },
     },
     { 
       path: '/browse', 
       name: 'Browse', 
-      component: BrowseView
+      component: BrowseView,
+      meta: { requiresAuth: true },
     },
     { 
       path: '/song/:id', 
       name: 'SongDetail', 
       component: SongDetailView, 
-      props: true 
+      props: true,
+      meta: { requiresAuth: true },
     },
     {
       path: '/artist/:id',
       name: 'ArtistDetail',
       component: ArtistDetailView,
-      props: true 
+      props: true,
+      meta: { requiresAuth: true },
     }, 
     {
       path: '/album/:id',
       name: 'AlbumDetail',
       component: AlbumDetailView,
-      props: true 
+      props: true,
+      meta: { requiresAuth: true },
     }
   ],
 })
+
+// Navigation guard to check authentication
+router.beforeEach(async (to) => {
+  const userDataStore = useUserDataStore();
+
+  if (to.meta.requiresAuth && !userDataStore.userData) {
+    await userDataStore.getUserData();
+
+    if (!userDataStore.userData) {
+      // Redirect to home if not authenticated
+      return { name: 'home' };
+    }
+  }
+
+
+  return true;
+});
+
 
 export default router
