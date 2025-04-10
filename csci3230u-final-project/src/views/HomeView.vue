d821b2496f0d0c9506539542d80829aab2793675
David Garcia <49959037+garciadavid2000@users.noreply.github.com>
2025-04-09 22:10:32 -0400

    WDF-51 Use Bulma Hero Page and add Login button

--- csci3230u-final-project/src/views/HomeView.vue
+++ csci3230u-final-project/src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <script setup>
+import LoginButton from "@/components/LoginButton.vue";
 
+function handleLogin() {
+  window.location.href = "/api/login";
+}
 </script>
 
 <template>
+  <section class="hero is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-half is-offset-one-quarter">
+          <h1 class="title">🎶 Welcome To our Spotify Wrapper Project!</h1>
+          <p class="subtitle mt-5">🎹 This is a simple test of the Spotify API</p>
+          <p class="subtitle">Click the button below to log in with your Spotify account and start using the app.</p>
+          <p class="title mt-5">👇</p>
+            <LoginButton @click="handleLogin" />
+        </div>
+      </div>
+    </div>
+  </section>
   <div>
-    <h1>Spotify API Test</h1>
-    <a href="/api/login">Log in with Spotify</a>
   </div>
 </template>
